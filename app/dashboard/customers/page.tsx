c24f528812b14658c251e05fad81add1d7748dd3
zoom <zoom@zooms-MacBook-Pro.local>
2025-05-19 17:57:12 +0400

    Customers list add

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-  return <p>Customers Page</p>;
+  return  <div>
+    <h2>Customer Page</h2>
+<ul>
+    <li>Customer 1</li>
+    <li>Customer 2</li>
+    <li>Customer 3</li>
+    <li>Customer 4</li>
+</ul>
+  </div>
+  
+  ;
 }
