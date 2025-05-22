6769c6752ca17b402b40bdd52b155a597394faf7
zoom <zoom@zooms-MacBook-Pro.local>
2025-05-22 11:18:57 +0400

    version 2

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,12 @@
 export default function Page() {
   return  <div>
     <h2>Customer Page</h2>
-<ul>
-    <li>Customer 1</li>
-    <li>Customer 2</li>
-    <li>Customer 3</li>
-    <li>Customer 4</li>
-</ul>
+        <ul>
+            <li>Customer 1</li>
+            <li>Customer 2</li>
+            <li>Customer 3</li>
+            <li>Customer 4</li>
+        </ul>
   </div>
   
   ;
